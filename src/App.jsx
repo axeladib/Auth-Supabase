cdbfc44c35153fac96c9e3880bf68f54d8b9a150
axeladib <nabil298adib@gmail.com>
2023-10-08 14:45:04 +0800

    Update the authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 import { Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp";
 import { Container } from "react-bootstrap";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Container
@@ -19,8 +14,7 @@ function App() {
         <div className="w-100" style={{ maxWidth: "400px" }}>
           <Routes>
             <Route path="/" element={<SignUp />}></Route>
-            <Route path="/sign-up"></Route>
-            <Route path="/sign-in"></Route>
+            <Route path="/sign-in" element={<SignIn />}></Route>
           </Routes>
         </div>
       </Container>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { Form, Button, Card, Link } from "react-bootstrap";
 
 function SignIn() {
   return (
-    <div>SignIn</div>
-  )
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Login</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" required />
+            </Form.Group>
+
+            <div className="text-center mt-2">
+              <Button type="submit" className="w-50">
+                Login
+              </Button>
+            </div>
+          </Form>
+        </Card.Body>
+        <div className="w-100 text-center mt-2">
+          New User? <Link to={"/register"}>Register</Link>
+        </div>
+      </Card>
+    </>
+  );
 }
 
-export default SignIn
+export default SignIn;
