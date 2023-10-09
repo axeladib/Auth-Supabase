de531d7f7ca944a8dfed06948a63428963bf4ab4
axeladib <nabil298adib@gmail.com>
2023-10-09 20:15:06 +0800

    Authentication : Test the AuthRoute.jsx getSession() method

--- src/components/AuthRoute.jsx
+++ src/components/AuthRoute.jsx
@@ -12,16 +12,27 @@ const AuthRoute = () => {
   //TODO: Calling the function to get the session
   useEffect(() => {
     //TODO: Get the user session with the destructuring method of response with data or error
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    console.log(`get session response : ${session}`);
     //TODO: console.log the session
     //FIXME: Problem is here
     //TODO: Also listen to the event changes and get the session
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
     //FIXME : Problem is here
     //TODO: After that, use the subscription method to unsubscribe to preven data leaking
+    return () => subscription.unsubscribe();
     //TODO: console.log the session
     //TODO: Listen to the changers
   }, []);
 
   return session ? (
+    // TODO: This element will render either <DashboardMessages> when the URL ismessages", <DashboardTasks> at "/tasks", or null if it is "/"
     <Outlet />
   ) : (
     <Navigate to={"/sign-in"} replace state={{ path: location.pathname }} />
